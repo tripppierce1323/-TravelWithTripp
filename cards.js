c38b60facad8b57400d3e5884ae9a1836f78005a
tripppierce1323 <tripppierce1323@gmail.com>
2026-05-01 13:01:33 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,16 +1,40 @@
-window.cards = [
+const cards = [
   {
     name: "Amex Gold",
-    issuer: "American Express",
-    bonus: "90,000 points",
-    image: "https://upload.wikimedia.org/wikipedia/commons/3/30/American_Express_Gold_Card.png",
-    color: "gold"
+    image: "https://i.imgur.com/3yaf3YF.png",
+    bonus: "Great for groceries + restaurants",
+    rewards: "4x dining • 4x U.S. supermarkets",
+  },
+  {
+    name: "Amex Platinum",
+    image: "https://i.imgur.com/B7R8K9C.png",
+    bonus: "Luxury travel perks",
+    rewards: "5x flights • Lounge access",
   },
   {
     name: "Chase Sapphire Preferred",
-    issuer: "Chase",
-    bonus: "75,000 points",
-    image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Chase_Sapphire_Preferred_Card.png",
-    color: "blue"
-  }
+    image: "https://i.imgur.com/7b7Q3sO.png",
+    bonus: "Best starter travel card",
+    rewards: "Travel partners • Dining",
+  },
+  {
+    name: "Chase Sapphire Reserve",
+    image: "https://i.imgur.com/V3gXzKz.png",
+    bonus: "Premium Chase card",
+    rewards: "$300 travel credit • Lounge access",
+  },
+  {
+    name: "Capital One Venture X",
+    image: "https://i.imgur.com/YcK0p7Q.png",
+    bonus: "Simple premium travel",
+    rewards: "2x everywhere • Lounge access",
+  },
+  {
+    name: "Bilt Mastercard",
+    image: "https://i.imgur.com/tQK9L3P.png",
+    bonus: "Earn points on rent",
+    rewards: "Rent • Dining • Travel",
+  },
 ];
+
+export default cards;
