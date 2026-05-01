b50faab0a940fcd949d400a69a8bf02dcdce1624
tripppierce1323 <tripppierce1323@gmail.com>
2026-05-01 14:06:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-h
+function loadPage(page) {
+  fetch('pages/' + page)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('content').innerHTML = data;
+    });
+}
+
+// Load home page by default
+window.onload = () => loadPage('home.html');
