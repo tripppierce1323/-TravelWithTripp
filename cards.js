3f7ddee3c0306ded65333d6d9111e68d28b9423c
tripppierce1323 <tripppierce1323@gmail.com>
2026-04-30 23:02:21 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,1 +1,16 @@
-window.cards = [];
+window.cards = [
+  {
+    name: "Amex Gold",
+    issuer: "American Express",
+    bonus: "90,000 points",
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/30/American_Express_Gold_Card.png",
+    color: "gold"
+  },
+  {
+    name: "Chase Sapphire Preferred",
+    issuer: "Chase",
+    bonus: "75,000 points",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Chase_Sapphire_Preferred_Card.png",
+    color: "blue"
+  }
+];
