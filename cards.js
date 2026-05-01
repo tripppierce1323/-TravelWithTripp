01adb8aa3ada022d7e0681eba3978b5f8d7ddfb7
tripppierce1323 <tripppierce1323@gmail.com>
2026-04-30 22:45:57 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -3,12 +3,12 @@ window.cards = [
     name: "Amex Gold",
     issuer: "American Express",
     bonus: "90,000 points",
-    image: "https://i.imgur.com/8Km9tLL.png"
+    image: "https://creditcards.americanexpress.com/content/dam/amex/us/credit-cards/card-assets/american-express-gold-card.png"
   },
   {
     name: "Chase Sapphire Preferred",
     issuer: "Chase",
     bonus: "75,000 points",
-    image: "https://i.imgur.com/2nCt3Sbl.png"
+    image: "https://creditcards.chase.com/K-Marketplace/images/cards/sapphire-preferred-card.png"
   }
 ];
