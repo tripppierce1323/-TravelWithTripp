4b668fe163e62f20f43698625c20657ff6f8912a
tripppierce1323 <tripppierce1323@gmail.com>
2026-05-01 13:01:03 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,51 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-
-const cards = [
-  {
-    name: "Amex Gold",
-    image:
-      "https://icm.aexp-static.com/Internet/internationalcardshop/en_us/images/cards/gold-card.png",
-    bonus: "Great for groceries + restaurants",
-    rewards: "4x dining • 4x U.S. supermarkets",
-  },
-  {
-    name: "Amex Platinum",
-    image:
-      "https://icm.aexp-static.com/Internet/internationalcardshop/en_us/images/cards/platinum-card.png",
-    bonus: "Luxury travel perks",
-    rewards: "5x flights • Lounge access • Hotel credits",
-  },
-  {
-    name: "Chase Sapphire Preferred",
-    image:
-      "https://creditcards.chase.com/K-Marketplace/images/cardart/sapphire_preferred_card.png",
-    bonus: "Best starter travel card",
-    rewards: "Travel partners • Dining • Travel portal",
-  },
-  {
-    name: "Chase Sapphire Reserve",
-    image:
-      "https://creditcards.chase.com/K-Marketplace/images/cardart/sapphire_reserve_card.png",
-    bonus: "Premium Chase travel card",
-    rewards: "$300 travel credit • Lounge access",
-  },
-  {
-    name: "Capital One Venture X",
-    image:
-      "https://ecm.capitalone.com/WCM/card/products/venture-x-card-art.png",
-    bonus: "Simple premium travel setup",
-    rewards: "2x everywhere • Lounge access • Travel credit",
-  },
-  {
-    name: "Bilt Mastercard",
-    image:
-      "https://assets.biltrewards.com/images/card/bilt-mastercard.png",
-    bonus: "Earn points on rent",
-    rewards: "Rent • Dining • Travel partners",
-  },
-];
+import cards from "./cards";
 
 function App() {
   return (
@@ -82,9 +38,6 @@ function App() {
 
       <section className="cardsSection">
         <h3>Featured Travel Cards</h3>
-        <p className="sectionText">
-          Compare cards that unlock flights, hotels, lounges, and luxury travel.
-        </p>
 
         <div className="cardsGrid">
           {cards.map((card) => (
@@ -92,7 +45,7 @@ function App() {
               <h4>{card.name}</h4>
 
               <div className="cardImageWrap">
-                <img src={card.image} alt={card.name} />
+                <img src={card.image} />
               </div>
 
               <p className="bonus">{card.bonus}</p>
@@ -107,5 +60,4 @@ function App() {
   );
 }
 
-// 🔥 THIS IS WHAT FIXES THE BLANK SCREEN
 createRoot(document.getElementById("root")).render(<App />);
