a1b4079298d8000fcbacd6994d9b7a1601f55fe7
tripppierce1323 <tripppierce1323@gmail.com>
2026-05-03 16:10:22 -0400

    Update cards.js

--- data/cards.js
+++ data/cards.js
@@ -466,7 +466,7 @@ const creditCards = [
     premium: true,
     beginnerFriendly: false,
     business: false
-  }
+  },
   // =====================
   // CHASE ULTIMATE REWARDS
   // =====================
